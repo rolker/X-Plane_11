657851141c547bcc71a3683dfd6af2013c3aa7b3
Roland Arsenault <roland@ccom.unh.edu>
2021-02-18 16:12:09 -0500

    Update DHC2_primer.lua
    Fixed typo

--- Resources/plugins/FlyWithLua/Scripts/DHC2_primer.lua
+++ Resources/plugins/FlyWithLua/Scripts/DHC2_primer.lua
@@ -1,14 +1,14 @@
 if PLANE_ICAO == "DHC2" then
 --wobble pump to axis
 DataRef("primer_value", "thranda/cockpit/actuators/primer", "writeable")
-DataRef("speedbreak_axis", "sim/joystick/joy_mapped_axis_value", "readonly", 14)
-DataRef("speedbreak_axis_available", "sim/joystick/joy_mapped_axis_avail", "readonly", 14)
+DataRef("speedbrake_axis", "sim/joystick/joy_mapped_axis_value", "readonly", 14)
+DataRef("speedbrake_axis_available", "sim/joystick/joy_mapped_axis_avail", "readonly", 14)
 
 last_primer_value = 0.0
 
 function primer_axis()
-  if speedbreak_axis_available == 1 then
-	last_primer_value = speedbreak_axis
+  if speedbrake_axis_available == 1 then
+	last_primer_value = speedbrake_axis
   end
   primer_value = last_primer_value
 end
